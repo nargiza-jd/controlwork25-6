ffeb4ea92cdaceae5e79317c32590a84242d2ea7
nargiza-jd <supabaevanargiza@gmail.com>
2025-07-05 13:52:42 +0600

    The code for the Main class has been written for this project

--- src/kg/attractor/java/Main.java
+++ src/kg/attractor/java/Main.java
@@ -1,13 +1,13 @@
 package kg.attractor.java;
 
+import kg.attractor.java.server.PatientServer;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            new Lesson46Server("localhost", 8089
-            ).start();
+            new PatientServer("localhost", 8089).start();
         } catch (IOException e) {
             e.printStackTrace();
         }

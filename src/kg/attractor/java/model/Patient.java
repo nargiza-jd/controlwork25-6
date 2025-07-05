5e86fe1ca18ab46609dc8b094e02344b735d45db
nargiza-jd <supabaevanargiza@gmail.com>
2025-07-05 14:49:19 +0600

    Code written for Patient

--- src/kg/attractor/java/model/Patient.java
+++ src/kg/attractor/java/model/Patient.java
@@ -1,4 +1,59 @@
 package kg.attractor.java.model;
 
+import java.time.LocalTime;
+
 public class Patient {
+    private LocalTime time;
+    private String fullName;
+    private String type;
+    private String symptoms;
+
+    public Patient(LocalTime time, String fullName, String type, String symptoms) {
+        this.time = time;
+        this.fullName = fullName;
+        this.type = type;
+        this.symptoms = symptoms;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSymptoms() {
+        return symptoms;
+    }
+
+    public void setSymptoms(String symptoms) {
+        this.symptoms = symptoms;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "time=" + time +
+                ", fullName='" + fullName + '\'' +
+                ", type='" + type + '\'' +
+                ", symptoms='" + symptoms + '\'' +
+                '}';
+    }
 }
